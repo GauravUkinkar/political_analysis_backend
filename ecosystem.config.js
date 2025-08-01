ad195de3fa7922781fcc9fd6a9e6d99fd63f065a
Ketan Dudka <ketandudka566123@gmail.com>
2025-08-01 12:29:35 +0530

    push

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,9 +1,12 @@
 module.exports = {
   apps: [
     {
-      name: "political_stat_backend",
-      script: "./index.js",
-      node_args: "--max-old-space-size=1024"
+      name: "political_stat_backend",       // name shown in `pm2 list`
+      script: "./index.js",                 // your entry point file
+      node_args: "--max-old-space-size=1024", // optional: allocates 1GB heap
+      env: {
+        NODE_ENV: "production",             // environment variable
+      }
     }
   ]
 };
